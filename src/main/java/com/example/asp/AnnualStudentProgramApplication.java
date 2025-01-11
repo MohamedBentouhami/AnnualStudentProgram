1aecb1267dd4e0c37fd2ea91a5c4154627a2ca6a
Mohamed <mohamedbentouhami512@gmail.com>
2025-01-11 14:39:38 +0100

    set up AnnualStudentProgram project

---
+++ src/main/java/com/example/asp/AnnualStudentProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.asp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualStudentProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnualStudentProgramApplication.class, args);
+	}
+
+}
