6208640f1a38e3e2b4696a82952e13bedcb86e75
Mohamed <mohamedbentouhami512@gmail.com>
2025-01-12 14:25:55 +0100

    add course form

--- src/main/java/com/example/asp/web/CourseController.java
+++ src/main/java/com/example/asp/web/CourseController.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.asp.business.Asp;
+import com.example.asp.model.Course;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class CourseController {
 
     private Asp business;
 
-    public CourseController(){
+    public CourseController() {
         business = new Asp();
     }
 
@@ -21,12 +25,18 @@ public class CourseController {
     public String coursesPage(Model model) {
         var courses = business.getCourses();
         model.addAttribute("courses", courses);
+        model.addAttribute("course", new Course());
         return "courses";
     }
 
     @GetMapping("/courses/{idCourse}")
-    public String course(@PathVariable String idCourse){
-        System.out.println(idCourse);
+    public String course(@PathVariable String idCourse) {
         return "course";
+    }
+
+    @PostMapping("/create-course")
+    public String createCourse(Course course) {
+        log.info(course.toString());
+        return "home";
     }
 }
